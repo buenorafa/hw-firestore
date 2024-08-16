0411d8c7188f6626d8620a7643c437490982c38d
buenorafa <rafaelbuenooo@outlook.com>
2024-08-15 21:25:23 -0300

    Alterações feitas em sala

--- src/app/shared/services/usuario-rest.service.ts
+++ src/app/shared/services/usuario-rest.service.ts
@@ -1,17 +1,35 @@
 import { Injectable } from '@angular/core';
-import {Usuario} from "../model/usuario";
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import { Usuario } from '../model/usuario';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsuarioRestService {
-
   URL_USUARIOS = 'http://localhost:3000/usuarios';
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   listar(): Observable<Usuario[]> {
     return this.httpClient.get<Usuario[]>(this.URL_USUARIOS);
+  }
+
+  inserir(usuario: Usuario): Observable<Usuario> {
+    return this.httpClient.post<Usuario>(this.URL_USUARIOS, usuario);
+  }
+
+  remover(usuario: Usuario): Observable<any> {
+    return this.httpClient.delete(`${this.URL_USUARIOS}/${usuario.id}`);
+  }
+
+  atualizar(usuario: Usuario): Observable<Usuario> {
+    return this.httpClient.put<Usuario>(
+      `${this.URL_USUARIOS}/${usuario.id}`,
+      usuario
+    );
+  }
+
+  buscarPorId(id: string): Observable<Usuario> {
+    return this.httpClient.get<Usuario>(`${this.URL_USUARIOS}/${id}`);
   }
 }
--- src/app/usuario/cadastro-usuario/manter-usuario.component.ts
+++ src/app/usuario/cadastro-usuario/manter-usuario.component.ts
@@ -1,29 +1,35 @@
-import { Component } from '@angular/core';
-import {Usuario} from "../../shared/model/usuario";
-import {ActivatedRoute, Router} from "@angular/router";
-import {UsuarioService} from "../../shared/services/usuario.service";
-import Swal from "sweetalert2";
-import {MensagemSweetService} from "../../shared/services/mensagem-sweet.service";
+import { afterNextRender, Component } from '@angular/core';
+import { Usuario } from '../../shared/model/usuario';
+import { ActivatedRoute, Router } from '@angular/router';
+// import { UsuarioService } from '../../shared/services/usuario.service';
+import Swal from 'sweetalert2';
+import { MensagemSweetService } from '../../shared/services/mensagem-sweet.service';
+import { UsuarioRestService } from 'src/app/shared/services/usuario-rest.service';
 
 @Component({
   selector: 'app-cadastro-usuario',
   templateUrl: './manter-usuario.component.html',
-  styleUrl: './manter-usuario.component.scss'
+  styleUrl: './manter-usuario.component.scss',
 })
 export class ManterUsuarioComponent {
-
   usuario = new Usuario('1', '', 0);
   modoEdicao = false;
 
-  constructor(private roteador: Router, private rotaAtual: ActivatedRoute,
-              private usuarioService: UsuarioService, private mensagemService: MensagemSweetService) {
+  constructor(
+    private roteador: Router,
+    private rotaAtual: ActivatedRoute,
+    private usuarioService: UsuarioRestService,
+    private mensagemService: MensagemSweetService
+  ) {
     const idParaEdicao = rotaAtual.snapshot.paramMap.get('id');
+    console.log(idParaEdicao);
     if (idParaEdicao) {
       this.modoEdicao = true;
-      const usuarioAEditar = usuarioService.listar().find(usuario => usuario.id == idParaEdicao);
-      if (usuarioAEditar) {
-        this.usuario = usuarioAEditar;
-      }
+      usuarioService.buscarPorId(idParaEdicao).subscribe({
+        next: (usuarioRecebido) => (this.usuario = usuarioRecebido),
+      });
+      console.log(this.usuario);
+      // usuarioService.atualizar(this.usuario);
     }
   }
 
@@ -31,9 +37,10 @@ export class ManterUsuarioComponent {
     if (!this.modoEdicao) {
       try {
         this.usuarioService.inserir(this.usuario);
+
         this.roteador.navigate(['listagem-usuarios']);
         this.mensagemService.sucesso('Usuário cadastrado com sucesso.');
-      } catch (e: any){
+      } catch (e: any) {
         this.mensagemService.erro(e.message);
       }
     }
